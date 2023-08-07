08270a542ae0701801f3020667e6c9dc7474c253
Gibi <gibialexmathew@gmail.com>
2023-08-08 04:57:20 +0530

    Fits commit of assignments

---
+++ src/exercises/DoWhileOdd.java
@@ -0,0 +1,16 @@
+package exercises;
+
+public class DoWhileOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =1;
+		
+		do {
+			System.out.println(i);
+			i=i+2;
+		}while(i<=10);
+
+	}
+
+}
---
+++ src/exercises/SumOfNumbers.java
@@ -0,0 +1,17 @@
+package exercises;
+
+public class SumOfNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum = 0;
+		
+		for(int i = 1; i <= 100; i++) {
+			sum += i;
+		}
+		
+		System.out.println("The sum of numbers from 1 to 100 is: " + sum);
+	}
+
+}
