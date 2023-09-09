cb3c598cad4dfae83c34414acaa7a8fdf3d5a29d
Gibi <gibialexmathew@gmail.com>
2023-09-09 23:19:31 +0530

    New Class added

---
+++ src/javaassignments/Quiz1.java
@@ -0,0 +1,15 @@
+package javaassignments;
+
+public class Quiz1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String letters = "";
+		while(letters.length() != 2) {
+			letters += "a";
+			System.out.println(letters);
+		}
+
+	}
+
+}
